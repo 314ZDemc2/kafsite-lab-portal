b0d50b69601051bed4209c1e8a03c74596e41a66
Ed <ed@leb.viti.kaf21>
2025-11-22 18:12:54 +0200

    Feature: Configured routes/web.php and PageController methods for all menu items (About, Contact, Gallery, News).

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,31 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+// kafsite/routes/web.php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\NewsController;
+use App\Http\Controllers\GalleryController;
+
+/* Головна сторінка (Вимога 1) */
+Route::get('/', function () {
+    // Поки що повертаємо лише шаблон. Логіку додамо пізніше.
+    return view('welcome');
+})->name('home');
+
+
+/* Статичні сторінки (Меню) */
+// 1. Про сайт
+Route::get('about', [PageController::class, 'about'])->name('about');
+// 2. Контакти (з картою Google)
+Route::get('contact', [PageController::class, 'contact'])->name('contact');
+
+/* Динамічні сторінки (Меню) */
+// 3. Галерея картинок
+Route::get('gallery', [GalleryController::class, 'index'])->name('gallery.index');
+// 4. Новини
+Route::resource('news', NewsController::class); // Створює всі маршрути для новин (список, деталі, CRUD)
 
 Route::get('/', function () {
     return view('welcome');
